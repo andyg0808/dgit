08b80efe406a7a47d08cb0f3548e55f7a638bd83
Andrew Gilbert <andrewg800@gmail.com>
2012-01-09 23:28:49 -0800

    Fixed bug in dgit-locations (forgot all the thens after the ifs and elifs)

--- bin/dgit-locations.sh
+++ bin/dgit-locations.sh
@@ -8,36 +8,44 @@ conffolder()
 {
 	#Check if the config file is in the current directory
 	if [ -e "./$confname" ]
+	then
 		cat "./$confname"
 	
 	#Check if the config file is in the user's home directory
 	elif  [ -e ~/"$confname" ]
+	then
 		cat ~/"$confname"
 	
 	#Check if the config file is in the directory where this script was
 	#symlinked from
 	elif [ -e "$(dirname $0)/$confname" ]
+	then
 		cat "$(dirname $0)/$confname"
 	
 	
 	#Check if the config file is in the script files directory
 	elif [ -e "$SOURCE/$confname" ]
+	then
 		cat "$SOURCE/$confname"
 	
 	#Check if the config file is in the /usr/local/etc directory
 	elif [ -e "/usr/local/etc/$confname" ]
+	then
 		cat "/usr/local/etc/$confname"
 	
 	#Check if the config file is in the /usr/local/etc/dgit directory
 	elif [ -e "/usr/local/etc/dgit/$confname" ]
+	then
 		cat "/usr/local/etc/dgit/$confname"
 		
 	#Check if the config file is in the /etc directory
 	elif [ -e "/etc/$confname" ]
+	then
 		cat "/etc/$confname"
 		
 	#Check if the config file is in the /etc/dgit directory
 	elif [ -e "/etc/dgit/$confname" ]
+	then
 		cat "/etc/dgit/$confname"
 		
 
