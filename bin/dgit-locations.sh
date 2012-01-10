ea7e9f29432070276c51c92de5c9afa3eb0d2459
Andrew Gilbert <andrewg800@gmail.com>
2012-01-09 22:10:13 -0800

    Designed config search function

---
+++ bin/dgit-locations.sh
@@ -0,0 +1,49 @@
+#The name of the configuration file that will be searched
+confname="newstools.conf"
+
+#Determines the location of the config file and returns its contents.
+#Expects $SOURCE to be set, which is the directory in which the script files are
+#installed
+conffolder()
+{
+	#Check if the config file is in the current directory
+	if [ -e "./$confname" ]
+		cat "./$confname"
+	
+	#Check if the config file is in the user's home directory
+	elif  [ -e ~/"$confname" ]
+		cat ~/"$confname"
+	
+	#Check if the config file is in the directory where this script was
+	#symlinked from
+	elif [ -e "$(dirname $0)/$confname" ]
+		cat "$(dirname $0)/$confname"
+	
+	
+	#Check if the config file is in the script files directory
+	elif [ -e "$SOURCE/$confname" ]
+		cat "$SOURCE/$confname"
+	
+	#Check if the config file is in the /usr/local/etc directory
+	elif [ -e "/usr/local/etc/$confname" ]
+		cat "/usr/local/etc/$confname"
+	
+	#Check if the config file is in the /usr/local/etc/dgit directory
+	elif [ -e "/usr/local/etc/dgit/$confname" ]
+		cat "/usr/local/etc/dgit/$confname"
+		
+	#Check if the config file is in the /etc directory
+	elif [ -e "/etc/$confname" ]
+		cat "/etc/$confname"
+		
+	#Check if the config file is in the /etc/dgit directory
+	elif [ -e "/etc/dgit/$confname" ]
+		cat "/etc/dgit/$confname"
+		
+
+	else
+		#Die with an error
+		exit 50
+	fi
+	
+}
